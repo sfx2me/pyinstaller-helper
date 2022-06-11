a68f415908a953f372fcf04218eb8e0bc6a93e76
us3rname101 <95178906+us3rname101@users.noreply.github.com>
2022-06-11 23:25:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import os, shutil, time
+
+import os, shutil, time, subprocess
+try:
+  subprocess.check_output("pip show pyinstaller")
+except subprocess.CalledProcessError:
+  os.system("pip install pyinstaller")
 print("Drag and drop your py file")
 pyPath = input()
 print("Drag and Drop your ico file")
