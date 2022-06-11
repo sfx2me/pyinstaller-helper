b5505715c7081b2a2c5d268231c50a1c23b3b4dd
us3rname101 <95178906+us3rname101@users.noreply.github.com>
2022-06-11 23:27:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import os, shutil, time, subprocess
 try:
-  subprocess.check_output("pip show pyinstaller")
+  subprocess.check_output("pip show pyinstaller",shell=True,stderr=subprocess.STDOUT)
 except subprocess.CalledProcessError:
   os.system("pip install pyinstaller")
 print("Drag and drop your py file")
