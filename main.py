4d6800e2f83ec1cb4f051153a6fa046fc2b7a6d7
Icode4u <88455386+sfx2me@users.noreply.github.com>
2022-06-11 15:44:12 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import os, shutil, time
+print("Drag and drop your py file")
+pyPath = input()
+print("Drag and Drop your ico file")
+ico = input()
+os.system(f'pyinstaller "{pyPath}" --icon "{ico}" --onefile')
+time.sleep(0.2)
+os.remove("main.spec")
+shutil.rmtree("build")
+print("Succesfully created your exe!")
